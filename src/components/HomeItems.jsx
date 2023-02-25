4e722e75255f341b62a43a509591a9b7bbc0aa4c
Pulak <pulakkumarghosh2001@gmail.com>
2023-02-25 09:34:32 +0530

    HomeScreen

--- src/components/HomeItems.jsx
+++ src/components/HomeItems.jsx
@@ -3,12 +3,35 @@ import React, { Component } from 'react'
 export class HomeItems extends Component {
   render() {
     return (
-      <div className="grid text-center">
-        <div className="g-col-6">
-            HHHH
+      <div className="text-center mx-5 d-flex flex-column flex-lg-row flex-md-row justify-content-around">
+        <div className="g-col-4 p-3 m-3">
+            <div className="d-flex flex-row">
+              <img src="https://static.wixstatic.com/media/485cee_b1223458cd414136860b3bd7341639dd~mv2.gif" alt="" />
+              <p className="text-primary fw-bold fs-3 my-auto ml-5">Conference Publications:</p>
+            </div>
+            <ul>
+              <li style={{textAlign: 'justify'}}>
+              We have a proposal for Springer Book Proceedings, Full length papers presented in  ICMMAM-2023 will undergo a peer review process and selected papers will subsequently be published in Springer Book Proceedings.
+              </li>
+              <li style={{textAlign: 'justify'}}>
+              Full length papers presented in  ICMMAM-2018 will undergo a peer review process and selected papers will subsequently be published in special issues of the following journals:
+              <ol>
+                <li value="1"><a href="">International Journal of Biomathematics and System Biology</a></li>
+                <li><a href="">Bulletin of Calcutta Mathematical Society/Journal of Calcutta Mathematical Society</a></li>
+              </ol>
+              </li>
+            </ul>
+            <p style={{textAlign: 'justify'}}>Authors of chosen papers may be required to correct/modify/update their papers according to the feedback received in the peer review. The decision of the reviewers will be final.</p>
         </div>
-        <div className="g-col-6">
-            bhbh
+        <div className="g-col-4"></div>
+        <div className="g-col-4 p-3 m-3">
+            <div className="d-flex flex-row">
+              <img src="https://static.wixstatic.com/media/485cee_b1223458cd414136860b3bd7341639dd~mv2.gif" alt="" />
+              <p className="text-primary fw-bold fs-3 my-auto ml-5">Call For Papers:</p>
+            </div>
+            <p className="text-danger fw-200" style={{textAlign: 'justify', backgroundImage: "url('https://static.wixstatic.com/media/485cee_36bf522e2ea44e609ea36c682389b65d~mv2.gif')", backgroundRepeat: 'no-repeat'}}>
+            Papers pertaining to conference areas of interest, as listed in “Aims and Scopes”, are invited for ICMAAM-2018. The authors are requested to submit their original full length research papers which are not being under consideration for publication by any other journal or conference, latest by April 30th, 2023 at the email: icmaam2023@gmail.com
+            </p>
         </div>
       </div>
     )
--- src/components/RegistrationDet.jsx
+++ src/components/RegistrationDet.jsx
@@ -5,7 +5,7 @@ export class RegistrationDet extends Component {
   render() {
     return (
       <div className="text-center m-5 d-flex flex-column flex-lg-row flex-md-row justify-content-around" style={{backgroundImage: "url('https://static.wixstatic.com/media/9060071fa0ea43ba92de11348b5a9c4f.jpg/v1/fill/w_977,h_501,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/9060071fa0ea43ba92de11348b5a9c4f.jpg')"}}>
-        <div className="g-col-4 bg-primary p-5 rounded m-3">
+        <div className="col-4 bg-primary p-5 rounded m-3">
             <h2 style={{color: 'white'}}>Registration Fees:</h2>
             <p style={{color: 'white'}}>Faculty/ Academician: Rs. 4,000/-(Indian)</p>
             <p style={{color: 'white'}}>Research Scholar: Rs. 3,000/-(Indian)</p>
@@ -14,8 +14,8 @@ export class RegistrationDet extends Component {
             <p style={{color: 'white'}}>Research Scholar: $ 200 (Foreign)</p>
             <p style={{color: 'white'}}>Accompanying Person: $ 200 (Foreign)</p>
         </div>
-        <div className="g-col-4"></div>
-        <div className="g-col-4">
+        <div className="col-4"></div>
+        <div className="col-4">
             <div className="bg-warning p-3 rounded m-3">
                 <h2>Instructions:</h2>
                 <p>Step 1: Click below and download the registration form.</p>

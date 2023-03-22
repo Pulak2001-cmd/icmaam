68aa993641d43776cca5ecea7eaad6c2a66c1ae1
Pulak <pulakkumarghosh2001@gmail.com>
2023-03-23 02:13:24 +0530

    More update

--- src/components/RegistrationDet.jsx
+++ src/components/RegistrationDet.jsx
@@ -5,7 +5,7 @@ export class RegistrationDet extends Component {
   render() {
     return (
       <div className="m-5 d-flex flex-column flex-lg-row flex-md-row justify-content-around align-items-center" style={{backgroundImage: "url('https://static.wixstatic.com/media/9060071fa0ea43ba92de11348b5a9c4f.jpg/v1/fill/w_977,h_501,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/9060071fa0ea43ba92de11348b5a9c4f.jpg')",  backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
-        <div className="g-col-4 bg-primary p-2 rounded m-3">
+        <div className="g-col-4 bg-primary p-1 p-lg-3 p-md-3 rounded m-3">
             <h2 style={{color: 'white'}}>Registration Fees:</h2>
             <ol style={{color: 'white', fontSize: 16}}>
               <li>

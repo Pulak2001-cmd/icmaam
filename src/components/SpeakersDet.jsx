b38ceda77b7df86b56db6f1ae993acdc84170d5b
Pulak <pulakkumarghosh2001@gmail.com>
2023-03-23 01:52:25 +0530

    Speakers Update

--- src/components/SpeakersDet.jsx
+++ src/components/SpeakersDet.jsx
@@ -4,23 +4,42 @@ export class SpeakersDet extends Component {
   constructor(props) {
     super(props);
     this.state = {
+        committe: [
+          "Prof. Odo Diekmann (Utrecht University, Netherlands)",
+          "Prof. Gaston N&#39;Guerekata (M.S.U, USA)",
+          "Prof. David Greenhalgh (U.S.G, Scotland)",
+          "Prof. Stacey Smith? (U.O, Canada)",
+          "Prof. Gergely Röst (University of Szeged, Hungary)",
+          "Prof. Miroslaw Lachowicz (W.U, Poland)",
+          "Prof. Jocirei Dias Ferreira (The Federal University of Mato Grosso – UFMT)",
+          "Prof. Shigui Ruan (U.M.G, Florida, USA)",
+          "Prof. Xianbing Cao (B.T.B.U, China)",
+          "Prof. Igor Schreiber (U.C.T, Czech Republic)",
+          "Prof. Xue-Zhi Li (Henan Normal University, China)",
+          "​Prof. Leo Creedon (ATU, Ireland)",
+          "Dr. Attila Dénes (University of Szeged, Hungary)",
+          "Prof. Evgenii Khailov (Lomonosov Moscow State University Russia)",
+          "Prof. Ellina Grigorieva (Texas Women University, United States of America)",
+          "Prof. El Kinani El Hassan (Université Moulay Ismail,Morocco)",
+          "Prof. Zbigniew Bartosiewicz (Bialystok University of Technology, Poland)",
+          "Prof. Ujjal Kumar Deb, CUET, Bangladesh.",
+          "Dr. Robert Hakel (Czech Academy of Sciences, Czech Republic)",
+          "Prof. Ali Moussaoui ( Abou Bakr Belkaid University of Tlemcen , Algeria)",
+          "Prof. Haider Ali Biswas (Science Engineering and Technology School, Bangladesh)",
+        ],
         international: [
-            "Prof. Gaston N'Guerekata (M.S.U, USA)",
-            "Prof. Miroslaw Lachowicz (U.W, Poland)",
-            "Prof. Yasuhiro Takeuchi (A.G.U, Japan)",
-            "Prof. Andreas Dress (F.U, German)",
-            "Prof. Erkay Savas (S.U, Turkey)",
-            "Prof. Ezio Venturino (T.U, Italy)",
-            "Prof. Xianbing Cao (B.T.B.U, China)",
-            "Dr. Yanhui Zhang (C.U.H.K, China)",
-            "Prof. V. M. De La Vega (Mexico)",
-            "Dr. Piotr Szewczak (C.S.W.U, Poland)",
-            "Prof. Urszula Foryś(U.W, Poland)", 
-            "Prof. Jiling Cao (A.U.T, New Zealand)",
-            "Prof. Ahmed Elaiw (K.A.U, UAE)",
-            "Prof. Igor Schreiber (U.C.T, Czech Republic)",
-            "Dr. Zbigniew Bartosiewicz (T.U.B, Poland)",
-            "Prof. Alev Kanibir (Turkey)",
+          "Prof. Gaston N&#39;Guerekata (M.S.U, USA)",
+          "Prof. David Greenhalgh (U.S.G, Scotland, UK)",
+          "Prof. Stacey Smith? (U.O, Canada)",
+          "Prof. Gergely Röst (University of Szeged, Hungary)",
+          "Prof. Miroslaw Lachowicz (W.U, Poland)",
+          "Prof. Igor Schreiber (U.C.T, Czech Republic)",
+          "Prof. Shigui Ruan (U.M.G, Florida, USA)",
+          "Dr. Attila Dénes (University of Szeged, Hungary)",
+          "Prof. Zbigniew Bartosiewicz (Bialystok University of Technology, Poland)",
+          "Prof. Ujjal Kumar Deb, CUET, Bangladesh.",
+          "Dr. Robert Hakel (Czech Academy of Sciences, Czech Republic)",
+          "Prof. Haider Ali Biswas (Science Engineering and Technology School, Bangladesh)",
         ],
         national: [
             "Prof. K. H. Paranjape (I.I.S.E.R, Mohali)", 
@@ -42,6 +61,12 @@ export class SpeakersDet extends Component {
     return (
       <div  className="m-5" style={{backgroundImage: "url('https://static.wixstatic.com/media/485cee_1fdf8218c0804eb6a86d963fe13643a6~mv2.jpg/v1/fill/w_480,h_543,al_c,q_80,enc_auto/485cee_1fdf8218c0804eb6a86d963fe13643a6~mv2.jpg')", backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
         <div className="d-flex text-left flex-column m-4 p-3">
+            <h3 className="text-danger">Scientific Advisory Committee (International)</h3>
+            {this.state.committe.map((i, index)=> {
+                return <p className="m-0 text-light fw-bold" index={index}>{i}</p>
+            })}
+        </div>
+        <div className="d-flex text-left flex-column m-4 p-3">
             <h3 className="text-danger">Confirmed Plenary and Invited Speakers (International)</h3>
             {this.state.international.map((i, index)=> {
                 return <p className="m-0 text-light fw-bold" index={index}>{i}</p>
@@ -49,9 +74,10 @@ export class SpeakersDet extends Component {
         </div>
         <div className="d-flex text-left flex-column m-4 p-3">
             <h3 className='text-danger'>Confirmed Plenary and Invited Speakers (National)</h3>
-            {this.state.national.map((i, index)=> {
+            {/* {this.state.national.map((i, index)=> {
                 return <p className="m-0 text-light fw-bold" index={index}>{i}</p>
-            })}
+            })} */}
+            <img src="/tba.png" height={330} width={330}/>
         </div>
       </div>
     )

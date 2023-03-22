f534eadaa3456b2c6cd3ff8e0f8a3602b40b60b1
Pulak <pulakkumarghosh2001@gmail.com>
2023-03-23 02:07:05 +0530

    Registration Details update

--- src/components/RegistrationDet.jsx
+++ src/components/RegistrationDet.jsx
@@ -4,15 +4,42 @@ import { Link } from 'react-router-dom'
 export class RegistrationDet extends Component {
   render() {
     return (
-      <div className="text-center m-5 d-flex flex-column flex-lg-row flex-md-row justify-content-around align-items-center" style={{backgroundImage: "url('https://static.wixstatic.com/media/9060071fa0ea43ba92de11348b5a9c4f.jpg/v1/fill/w_977,h_501,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/9060071fa0ea43ba92de11348b5a9c4f.jpg')"}}>
+      <div className=" m-5 d-flex flex-column flex-lg-row flex-md-row justify-content-around align-items-center" style={{backgroundImage: "url('https://static.wixstatic.com/media/9060071fa0ea43ba92de11348b5a9c4f.jpg/v1/fill/w_977,h_501,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/9060071fa0ea43ba92de11348b5a9c4f.jpg')",  backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
         <div className="g-col-4 bg-primary p-5 rounded m-3">
             <h2 style={{color: 'white'}}>Registration Fees:</h2>
-            <p style={{color: 'white'}}>Faculty/ Academician: Rs. 4,000/-(Indian)</p>
+            <ol style={{color: 'white', fontSize: 18}}>
+              <li>
+                  Foreign Participants: (Without Accomodation)
+                  <ul>
+                    <li>Faculty members - US $200</li>
+                    <li>Faculty members Third-World Countries - US $150</li>
+                    <li>Students/Research Scholars - US $100</li>
+                    <li>Students/Research Scholars (Third-World countries) - US $75</li>
+                  </ul>
+              </li>
+              <li>
+                  Indian Participants (Including 18% GST):
+                  <ul>
+                    <li>Faculty members with accomodation (Double Sharing) - Rs. 7000</li>
+                    <li>Faculty members without accomodation - Rs. 4000</li>
+                    <li>Students with accomodation (Double Sharing) - Rs. 5000</li>
+                    <li>Students without accomodation - Rs. 3000</li>
+                  </ul>
+              </li>
+              <li>
+                  JU Participants:
+                  <ul>
+                    <li>Scholars/students - Rs. 2500</li>
+                    <li>Faculty - Rs. 3500</li>
+                  </ul>
+              </li>
+            </ol>
+            {/* <p style={{color: 'white'}}>Faculty/ Academician (Without accomodation): Rs. 4,000/-(Indian)</p>
             <p style={{color: 'white'}}>Research Scholar: Rs. 3,000/-(Indian)</p>
             <p style={{color: 'white'}}>Accompanying Person: Rs. 3,000/-(Indian)</p>
             <p style={{color: 'white'}}>Faculty/ Academician: $ 300 (Foreign)</p>
             <p style={{color: 'white'}}>Research Scholar: $ 200 (Foreign)</p>
-            <p style={{color: 'white'}}>Accompanying Person: $ 200 (Foreign)</p>
+            <p style={{color: 'white'}}>Accompanying Person: $ 200 (Foreign)</p> */}
         </div>
         <div className="g-col-4"></div>
         <div className="g-col-4">

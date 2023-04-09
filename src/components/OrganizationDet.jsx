76b51508ca791fded8d3cf12c71c40a1a86dc450
Pulak <pulakkumarghosh2001@gmail.com>
2023-04-10 02:25:09 +0530

    data from sheet

--- src/components/OrganizationDet.jsx
+++ src/components/OrganizationDet.jsx
@@ -1,71 +1,27 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export class OrganizationDet extends Component {
     constructor(props) {
         super(props);
         this.state = {
             row1: [
-                "Prof. Odo Diekmann (Utrecht University, Netherlands)",
-                "Prof. Gaston N'Guerekata (M.S.U, USA)",
-                "Prof. David Greenhalgh (U.S.G, Scotland)",
-                "Prof. Stacey Smith? (U.O, Canada)",
-                "Prof. Gergely Röst (University of Szeged, Hungary)",
-                "Prof. M. S. Moslehian (Ferdowsi University of Massad, Iran)",
-                "Prof. Miroslaw Lachowicz (W.U, Poland)",
-                "Prof. Jocirei Dias Ferreira (The Federal University of Mato Grosso – UFMT)",
-                "Prof. Shigui Ruan (U.M.G, Florida, USA)",
-                "Prof. Jacek Chmieliński (Pedagogical University Of Krakow, Poland)",
-                "Prof. Xianbing Cao (B.T.B.U, China)",
-                "Prof. Igor Schreiber (U.C.T, Czech Republic)",
-                "Prof. Xue-Zhi Li (Henan Normal University, China)",
-                "Prof. Leo Creedon (ATU, Ireland)",
-                "Dr. Attila Dénes (University of Szeged, Hungary)",
-                "Prof. Evgenii Khailov (Lomonosov Moscow State University Russia)",
-                "Prof. Ellina Grigorieva (Texas Women University, United States of America)",
-                "Prof. El Kinani El Hassan (Université Moulay Ismail,Morocco)",
-                "Prof. Zbigniew Bartosiewicz (Bialystok University of Technology, Poland)",
-                "Prof. Ujjal Kumar Deb, CUET, Bangladesh.",
-                "Dr. Robert Hakel (Czech Academy of Sciences, Czech Republic)",
-                "Prof. Ali Moussaoui ( Abou Bakr Belkaid University of Tlemcen , Algeria)",
-                "Prof. Haider Ali Biswas (Science Engineering and Technology School, Bangladesh)",
+
             ],
             row2: [
-                "Prof. Gadadhar Misra (ISI, Bangalore)",
-                "Prof. Joydev Chattopadhyay, (ISI, Kolkata)",
-                "Prof. A K Misra (BHU)",
-                "Prof Jaydeb Sarkar (ISI, Bangalore)",
-                "Prof. Ranjan Kumar Mohanty (South Asian University)",
-                "Prof. Debashis Sarkar (CU)",
-                "Prof. Pritha Das (IIEST, Howrah)",
-                "Dr. Amit Kumar Chakraborty (Sikkim Central University, India)",
-                "Dr. Amit Kumar Ghosh (Emeritus Scientist, CSIR, BSIP, DST)",
-                "Prof. Adrijit Goswami (IIT, KGP)",
-                "Prof. M. Sundarajan (Mijoram University)",
-                "Prof. Apoorva Khare (IISC, Bengalore)"
+              
             ],
             row3: [
-                "Prof. Subhas Mandal (J.U, Kolkata)",
-                "Prof. Anup Bandyopadhyay (J.U, Kolkata)",
-                "Prof. Shamik Ghosh (J.U, Kolkata)",
-                "Prof. Bibhash Chandra Giri (J.U, Kolkata)",
-                "Prof. Buddhadeb Sau (J.U, Kolkata)",
-                "Prof. Farook Rahaman (J.U, Kolkata)",
-                "Prof. Alaka Das (J.U, Kolkata)",
-                "Dr. Sukhendu Kar (J.U, Kolkata)",
-                "Dr. Santosh Biswas (J.U, Kolkata)",
-                "Prof.Abhijit Lahiri (J.U, Kolkata)",
-                "Prof.Gopal Chandra Shit (J.U, Kolkata)",
-                "Prof. Ashish Kumar Sarkar (J.U, Kolkata)",
-                "Prof. Prabir Kumar Kundu (J.U, Kolkata)",
-                "Prof. Pratulananda Das (J.U, Kolkata)",
-                "Prof. Sudeshna Banerjee (J.U, Kolkata)",
-                "Prof. Sujit Kumar Sardar (J.U, Kolkata)",
-                "Dr. Manasi Mandal (J.U, Kolkata)",
-                "Dr. Nilanjana Mahata (J.U, Kolkata)",
-                "Dr.Molla Basir Ahamed (J.U, Kolkata)",
-                "Dr.Riddhick Birbonshi (J.U, Kolkata)",
-                "Dr. Sanjib Kumar Pal (J.U, Kolkata)",
+                
             ]
+        }
+    }
+    async componentDidMount(){
+        const url = 'https://script.google.com/macros/s/AKfycbzp9_t_8yM-NqJxEvx217pRreEpG1tuMtcFja3FgYzHyhig2oBwzLVM4iXZIVZB_cXS/exec?type=organization'
+        const response = await axios.get(url);
+        console.log(response.data);
+        if(response.status){
+            this.setState({row1: response.data.international, row2: response.data.national, row3: response.data.local});
         }
     }
   render() {
